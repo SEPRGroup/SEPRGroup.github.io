04d726cd8984b914c9d3b44218ac381982967771
Karl Sonley <kms526@york.ac.uk>
2014-01-22 04:10:03 +0000

    Added hash function
    Page now loads different div according to which div was open upon
    refresh

--- scripts.js
+++ scripts.js
@@ -1,4 +1,11 @@
 // JavaScript Document
+function check_hash(){
+	if(window.location.hash){
+		change_page(String(window.location.hash).substring(1));
+	}else{
+		return;
+	}
+}
 
 function change_page(div){
 	var allDivs = ["home_container", "coding_container", "gui_container", "testing_container", "downloads_container"];
@@ -6,8 +13,11 @@ function change_page(div){
 	for(var x = 0; x < allDivs.length; x ++){
 		if(allDivs[x] == divID){
 			document.getElementById(allDivs[x]).style.display="block";
+			window.location.hash = div;
+			console.log("Hello");
 		}else{
 			document.getElementById(allDivs[x]).style.display="none";
+			console.log(div);
 		}
 	}
 	
