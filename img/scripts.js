9e01e521825cb7cdf92035986638e5646c6f1ec4
Karl Sonley <kms526@york.ac.uk>
2014-01-23 18:32:10 +0000

    Made ing page image rotor

--- img/scripts.js
+++ img/scripts.js
@@ -1,11 +1,18 @@
 // JavaScript Document
-x = 1;
-
-function change_page(){
-	if (x == allDivs.length){
+function change_page(div){
+	var numDivs = 3;
+	var x = Number(String(div.substring(1)));
+	if (x == numDivs) {
 		x = 1;
+	}else {
+		x ++;
 	}
-	var allDivs = ["img1", "img2", "img3"];
-	document.getElementById(allDivs[x]).style.display="block";	
-	x ++
+	for(var i = 1; i <= numDivs; i ++){
+		var divID = "i" + String(i);
+		if(i == x){
+			document.getElementById(divID).style.display="block";
+		}else {
+			document.getElementById(divID).style.display="none";
+		}
+	}
 }
